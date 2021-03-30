c68a50d648e4de6e3f9629e491f647c98d8c2986
Gannon <gtraynor323@gmail.com>
2021-03-30 08:00:35 -0400

    getjson of info for parsing
    getjson of info for parsing

---
+++ getUserInfo.py
@@ -0,0 +1,17 @@
+# Tweepy is a Python library for accessing the Twitter API
+import tweepy
+
+# Using app keys and tokens from my developer app
+auth = tweepy.OAuthHandler("I8du75AluHGuyepB7O14ZNJyu", "Jg0IN5JiAOt684Ust0rQ5nNhpJYUUoRy1iRfbCVYEjTYX0rQ7i")
+auth.set_access_token("371302729-2qF49kYJRElsDGxXUH3mAZlJCRiuDEuNIeHnPrEY", "0Z5bvtg5ogPbfNrRyUDiBpfLZo8RwuNRvucXk9Ip0tToc")
+
+api = tweepy.API(auth)
+
+# Program asks for a Twitter username and prints the username as well as their follower count 
+# to the terminal
+
+
+userInput = input("\nPlease enter a Twitter username: ")
+user = api.get_user(userInput)
+
+print (user)
